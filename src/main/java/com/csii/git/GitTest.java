af74aec4658fb62394b64cd4f25f98ca942a4d41
weiwu <zhouguiming@csii.com>
2021-08-05 10:05:47 +0800

    first commit

---
+++ src/main/java/com/csii/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.csii.git;
+
+/**
+ * @Author：zhouguiming
+ * @Description：
+ * @Date： 2021/8/5 10:01
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
